183be9d765adf8cebc0bcb0517b256e5ce9b4577
mukku69 <programing0803@gmail.com>
2021-04-01 13:55:46 +0000

    [Finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9281227f472e61bb1c57ea85d0d0e130919219f8307176f6d92af8183325ec6bab705555515a9ea1a6ecd30912e239cc1232decab371b72ca5fe3c1433245239'
